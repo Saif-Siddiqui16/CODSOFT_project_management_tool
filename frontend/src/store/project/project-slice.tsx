46b21bf42613b2692af591155cb7d932c5a7b227
Mohd. Saif Siddiqui <siddiqui16saif@gmail.com>
2025-10-24 21:32:08 +0530

    frontend base_url changee

--- frontend/src/store/project/project-slice.tsx
+++ frontend/src/store/project/project-slice.tsx
@@ -1,6 +1,9 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+axios.defaults.baseURL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+axios.defaults.withCredentials = true;
 export interface Project {
   _id: string;
   title: string;
